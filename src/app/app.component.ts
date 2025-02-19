8a60f15968e16ff1670574d61a32e2f6b45837b5
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-20 03:00:39 +0700

    feat(scss): add default scss in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PorfileCardComponent } from "./components/porfile-card/porfile-card.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: 'app-root', // селектор главной страницы
+  
+  // тут прописываются зависимости
+  // которые нужны для создания главной страницы
+  imports: [
+    RouterOutlet, 
+    PorfileCardComponent
+  ],
+  templateUrl: './app.component.html', // html-шаблон главной страницы
+  styleUrl: './app.component.scss' // стили главной страницы
 })
 export class AppComponent {
   title = 'first-project';
