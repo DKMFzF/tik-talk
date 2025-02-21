b3c4759de6aea79bd60761d6d1d8756a54c450eb
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-21 18:24:03 +0700

    feat(search-page.component): add new components in page and add new path SearchPageComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
+/**
+ * Настройка роутинга приложения
+ */
 export const routes: Routes = [
-  {path: '', component: LoginPageComponent}
+  {path: '', component: SearchPageComponent},
+  {path: 'login', component: LoginPageComponent}
 ];
