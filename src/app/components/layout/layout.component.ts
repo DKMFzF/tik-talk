c5fbfaa961fa3c3f06301c6f046fe8286bb4082d
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-21 19:36:21 +0700

    feat(side-bar.component): add side-bar-components and option routing

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from '../side-bar/side-bar.component';
 
 @Component({
   selector: 'app-layout',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    SideBarComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
