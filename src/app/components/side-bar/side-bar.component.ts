bb95f342701e858744d95f8ac02799fd573b1f7b
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-23 02:06:36 +0700

    feat(side-bar.component): add profile-m

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { SvgComponent } from '../svg/svg.component';
+import { SubsCardComponent } from './subs-card/subs-card.component';
+import { RouterLink } from '@angular/router';
 
 /**
  * сайд-бар
  */
 @Component({
   selector: 'app-side-bar',
-  imports: [SvgComponent],
+  imports: [SvgComponent, SubsCardComponent, RouterLink],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
